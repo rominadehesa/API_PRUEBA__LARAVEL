d71ea700c5a9e0a1cd9c159eb92015022fc4b036
Romina Dehesa <rominadehesa@gmail.com>
2021-07-23 01:07:14 -0300

    ccambios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,7 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController{
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function x(){
-       return null;
-    }
+    //public function getItems(){
+    //  $items = HTTP::get('http://127.0.0.1:8000/api/items'); //PARA LEER LA API
+    //  $itemsArray = $items->json();
+    //  return view('welcome', compact('itemsArray'));
+    //}
 }
